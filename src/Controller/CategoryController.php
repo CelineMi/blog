46c693e79a028c4d8e3ea86b8787092aff535313
celine <celine.minaudier@gmail.com>
2018-11-12 12:38:46 +0100

    add show view on each article and category

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category", name="category")
+     */
+    public function index()
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category_show")
+     *
+     */
+    public function show(Category $category) :Response
+    {
+        return $this->render('category/show.html.twig', ['category'=>$category]);
+    }
+}
