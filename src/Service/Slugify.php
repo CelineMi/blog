7a1885de67a2d4e201b42745c9fc821512df7b50
celine <celine.minaudier@gmail.com>
2018-12-03 09:29:41 +0100

    add generate method

--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -15,7 +15,35 @@ class Slugify
     {
         $toReplace = [['é', 'è', 'ê'],'à','ï', 'ô', 'ç', [' ', '.', ',', '!', ';', ':','@']];
         $replaced = [['e'], 'a', 'i', 'o', 'c', ['_']];
-        $slug = trim(str_replace($toReplace, $replaced, $input));
+        $slug = preg_replace(',::.?\/!@#$%&*', '', $input);
+        $slug = preg_replace(' ', '_', $input);
+        $slug = preg_replace('èéê', 'e', $input);
+        $slug = preg_replace('à', 'a', $input);
+        $slug = preg_replace('ç', 'c', $input);
+        $slug = preg_replace('ï', 'i', $input);
+        $slug = preg_replace('ù', 'u', $input);
+        $slug = trim($slug);
+
+
+        // replace non letter or digits by -
+        $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // transliterate
+        if (function_exists('iconv'))
+        {
+            $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        }
+
+        // lowercase
+        $text = strtolower($text);
+
+        // remove unwanted characters
+        $text = preg_replace('#[^-\w]+#', '', $text);
+
+
 
         return $slug;
     }
